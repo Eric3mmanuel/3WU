1b1cece07edd0655757fb29fca13af4e7e5e53f2
Eric Emmanuel <115712268+Eric3mmanuel@users.noreply.github.com>
2025-10-15 23:55:22 +0300

    Delete wallet directory

--- wallet/assets/isAddressEqual-BwunfFKz.js
+++
@@ -1,1 +0,0 @@
-import{ae as e,af as t}from"./index-JRXeyCun.js";function d(s,r){if(!e(s,{strict:!1}))throw new t({address:s});if(!e(r,{strict:!1}))throw new t({address:r});return s.toLowerCase()===r.toLowerCase()}export{d as i};
